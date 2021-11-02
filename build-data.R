24facf2ca3770fbbd9cdca5083497fa9ce27c44a
Will Ebert <ebertwilliam@johndeere.com>
2021-11-02 08:55:00 -0500

    added ./data/monthly_prison_pop_released.rda

--- build-data.R
+++ build-data.R
@@ -5,17 +5,6 @@ library(lubridate)
 library(timetk)
 library(purrr)
 
-fill_dates <- function(data) {
-  
-  min_dt = max(data$admission_date)
-  max_dt = max(data$date_of_release)
-  
-  dates <- tibble(
-    ds = tk_make_timeseries(min_dt, max_dt, by = "month")
-  )
-
-  return(dates)
-}
 
 Iowa_Prison_Admissions <- read_csv("data/Iowa_Prison_Admissions.csv", 
                                    col_types = cols(`Admission Date` = col_date(format = "%m/%d/%Y"), 
@@ -25,29 +14,26 @@ Iowa_Prison_Admissions <- read_csv("data/Iowa_Prison_Admissions.csv",
 
 
 
-dates <- tibble(
-  ds = tk_make_timeseries(min(Iowa_Prison_Admissions$admission_date), max(Iowa_Prison_Admissions$date_of_release), by = "month")
-)
-
-tibble(
-  ds = tk_make_timeseries("2019-03-20", "2020-04-20", by = "month")
-)
-
 
 offender_dates <- Iowa_Prison_Admissions %>% 
   # head() %>% 
   filter(!is.na(admission_date),
          !is.na(date_of_release)) %>% 
-  group_by(offender_number, record_id) %>%
-  summarise(min_date = floor_date(min(admission_date), 'month'),
-            max_date = floor_date(max(date_of_release), 'month'))  %>% 
-  ungroup() %>% 
+  # group_by(offender_number, record_id) %>%
+  mutate(min_date = floor_date(admission_date, 'month'),
+            max_date = floor_date(date_of_release, 'month'))  %>% 
+  # ungroup() %>% 
   group_by(offender_number, record_id) %>% 
   mutate(dates_admitted = list(tibble(
     ds = tk_make_timeseries(min_date, max_date, by = "month")
   )))
 
-offender_dates %>% 
-  head(1) %>% 
-  tidyr::unnest() %>% 
-  mutate(ds = floor_date(ds, 'month'))
+offender_dates_by_month <- offender_dates %>%
+  select(offender_number, record_id, dates_admitted) %>% 
+  tidyr::unnest(cols = c(dates_admitted))
+
+monthly_prison_pop_released <- offender_dates_by_month %>% 
+  ungroup() %>% 
+  count(ds)
+
+save(monthly_prison_pop_released, file = "./data/monthly_prison_pop_released.rda")
