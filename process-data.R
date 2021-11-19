3e5e692d88aee5fb0b944b9fe12111e1bc715cf3
Will Ebert <ebertwilliam@johndeere.com>
2021-11-18 21:24:00 -0600

    add offense vairables to aggregation

--- process-data.R
+++ process-data.R
@@ -3,7 +3,6 @@ library(dplyr)
 library(janitor)
 library(lubridate)
 library(timetk)
-library(purrr)
 
 ## ---- Read Data ---- ##
 admissions <- read_csv("data/Iowa_Prison_Admissions.csv", 
@@ -53,24 +52,42 @@ unioned_df <- admission_dates %>%
     in_system_through_date = case_when(
       is.na(date_of_release) ~ floor_date(today(), 'month'),
       TRUE ~ date_of_release
-    )) %>% View
+    )) %>% 
+  distinct() %>%
+  
+  # Handles situtations where offenses are split into separate release periods
+  group_by(offender_number, admission_date) %>% 
+  filter(in_system_through_date == max(in_system_through_date)) %>%
+  ungroup() %>% 
+  
+  # collects offense codes into lists where offenders had multiple charges and serving them during the same period.
+  # group_by(offender_number, admission_date, in_system_through_date, sex, race_ethnicity) %>% 
+  # summarise(offense_codes = list(offense_code),
+  #           offense_classifications = list(offense_classification),
+  #           offense_types = list(offense_type),
+  #           offense_subtypes = list(offense_subtype)) %>% 
+  # ungroup() %>% 
   
   # Truncates dates to year-month for aggregation purposes
   # creates `visit_id` which is a unique identifier for each offender visit
   mutate(start_date = floor_date(admission_date, 'month'),
-         end_date = floor_date(in_system_through_date, 'month'),
-         visit_id = paste0(offender_number, as.integer(start_date))) %>% 
+       end_date = floor_date(in_system_through_date, 'month'),
+       visit_id = paste0(offender_number, as.integer(start_date))) %>% 
   select(offender_number, start_date, end_date, visit_id, sex, race_ethnicity, offense_code, offense_classification, offense_type, offense_subtype) %>% 
   
   # grouping visit_id and finding the min/max start dates 
   # sets the stage for exploading months in prison
-  group_by(visit_id, sex, race_ethnicity, offense_code, offense_classification, offense_type, offense_subtype) %>%
+  group_by(visit_id, sex, race_ethnicity) %>%
   summarise(min_date = min(start_date),
-            max_date = max(end_date)) %>%
-  ungroup() %>%
-  group_by(visit_id) %>%
+            max_date = max(end_date),
+            offense_codes = list(offense_code),
+            offense_classifications = list(offense_classification),
+            offense_types = list(offense_type),
+            offense_subtypes = list(offense_subtype)) %>%
+  ungroup() %>% 
   
   # expload dates in nested dataframe
+  group_by(visit_id) %>%
   mutate(dates_admitted = list(
     tibble(
       ds = tk_make_timeseries(min_date, max_date, by = "month")
@@ -86,7 +103,10 @@ unioned_df <- admission_dates %>%
 monthly_prison_pop <- unioned_df %>% 
   ungroup() %>% 
   filter(ds >= as.Date('2010-01-01')) %>% 
-  count(ds)
+  tidyr::unnest(cols = c(offense_codes,offense_classifications,offense_types,offense_subtypes)) %>% 
+  group_by(ds,sex, race_ethnicity, offense_codes,offense_classifications,offense_types,offense_subtypes) %>% 
+  summarise(n = n()) %>% 
+  ungroup()
 
 ## ---- Save .rda for Modeling ---- ##
 save(monthly_prison_pop, file = "./data/monthly_prison_pop.rda")
